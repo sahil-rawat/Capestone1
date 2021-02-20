2b461a42ae01dfd6e514a5cd6fb610df47faeb94
Sahil Singh Rawat <sahilsinghrawat1405@gmail.com>
2021-02-20 11:26:20 +0530

    configuring

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 const path = require('path');
 
-
 var app= express()
 
 app.use(express.static('res'));
@@ -10,5 +9,6 @@ app.use(express.static('res'));
 app.get('/home',function(req,res){
 	res.sendFile( __dirname + "/res/" + "index.html" )
 })
+
 
 app.listen(process.env.PORT || 8080)
