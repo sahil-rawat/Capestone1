0f9e6eab8baf4dc2c4a6e8c76634508e061acc0b
Sahil Singh Rawat <sahilsinghrawat1405@gmail.com>
2021-02-20 20:49:12 +0530

    Initialising and connecting database

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 var express = require('express');
-const path = require('path');
+const fs = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccount.json');
+
+fs.initializeApp({
+ credential: fs.credential.cert(serviceAccount)
+});
+const db = fs.firestore(); 
+
 
 var app= express()
+app.set("view engine", "ejs"); 
+app.set("views", __dirname + "/views/");
 
-app.use(express.static('res'));
 
 
-app.get('/home',function(req,res){
-	res.sendFile( __dirname + "/res/" + "index.html" )
+const getData = async function (id) {
+	const Project = await (await db.collection('Projects').doc(id).get()).data()
+	return Project
+}
+
+app.use(express.static('public'));
+getData('RlZhpFYwWaCyBC3z6s88').then(res=>{
+	console.log(res)
+	
+})
+
+app.get('/project/:id1',function(req,res){
+	console.log();
+
+	async function ren(){
+		s = await getData(req.params.id1)
+
+		await res.render('projDetails',{ 
+			data:JSON.stringify(s),
+		})
+	}
+	ren()	
+
 })
 
 
