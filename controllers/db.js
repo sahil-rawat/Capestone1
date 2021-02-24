53153ebfde5cc1df2f3636a84f0d6b5191c74ca5
Sahil Singh Rawat <sahilsinghrawat1405@gmail.com>
2021-02-24 20:34:31 +0530

    Editing Project Details Done

--- controllers/db.js
+++ controllers/db.js
@@ -4,7 +4,8 @@ const serviceAccount = require('../serviceAccount.json');
 fs.initializeApp({
     credential: fs.credential.cert(serviceAccount)
    });
-   const db = fs.firestore(); 
+   
+const db = fs.firestore(); 
 
 exports.getDetails= async id=> {
        const Project = await (await db.collection('Projects').doc(id).get()).data()
