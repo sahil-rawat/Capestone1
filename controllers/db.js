a1e6b60cbb6e0855712965b826612836444a9b11
Sahil Singh Rawat <sahilsinghrawat1405@gmail.com>
2021-03-14 19:27:29 +0530

    Deploy Branch

--- controllers/db.js
+++ controllers/db.js
@@ -1,10 +1,21 @@
 const fs = require('firebase-admin');
-const serviceAccount = require('../serviceAccount.json');
 
 
 
 fs.initializeApp({
-    credential: fs.credential.cert(serviceAccount)
+    credential: fs.credential.cert({
+        "type": process.env.type,
+        "project_id": process.env.project_id,
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": process.env.auth_uri,
+        "token_uri": process.env.token_uri,
+        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+        "client_x509_cert_url": process.env.client_x509_cert_url
+      }
+      )
    });
    
 
