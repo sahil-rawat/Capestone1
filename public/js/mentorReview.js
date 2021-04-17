f60e2c563a70588c0ce7ec7bb96ab4e6a4dfb573
NinadKanchan <56508313+NinadKanchan@users.noreply.github.com>
2021-04-17 16:23:08 +0530

    Basic Mentor Review Page Created

---
+++ public/js/mentorReview.js
@@ -0,0 +1,29 @@
+function test(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+}
+
+
+function showname(){
+	
+	id=event.target.name
+	document.getElementById(id).style.display='inline'
+}
+
+function hidename(){
+	
+	id=event.target.name
+	document.getElementById(id).style.display='none'
+}
+
