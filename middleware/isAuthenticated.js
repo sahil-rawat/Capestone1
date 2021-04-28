0146587b9fa26f71595209d25a260d3d75e62f98
NinadKanchan <56508313+NinadKanchan@users.noreply.github.com>
2021-04-28 23:50:09 +0530

    Commit Test

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,14 +1,1 @@
 
-const firebase = require('../controllers/auth')
-
-isAuthenticated=(req, res, next) =>{
-    var user = firebase.auth().currentUser;
-    if (user !== null) {
-      req.user = user;
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  }
-
-module.exports=isAuthenticated
