19d9b8c245a3f4f6c6f8583584294ab3d7c7c976
Nikhil Khandelwal <nikhil.khandelwal18@st.niituniversity.in>
2021-04-29 11:38:11 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,1 @@
-fuijofdfgfgupfglgffgsc 
-var firebase = require("firebase/app");
-var auth = require("firebase/auth");
 
-
-var firebaseConfig = {
-    apiKey: "AIzaSyDekw2vmrtSX0XmU570XhyZS1xcLu8fcIg",
-        authDomain: "capestone1-6b9f3.firebaseapp.com",
-        databaseURL: "https://capestone1-6b9f3-default-rtdb.firebaseio.com",
-        projectId: "capestone1-6b9f3",
-        storageBucket: "capestone1-6b9f3.appspot.com",
-        messagingSenderId: "772346577220",
-        appId: "1:772346577220:web:9d620f4e01393a0a5f5711",
-        measurementId: "G-EQ8PDHCQ6E"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  module.exports=firebase
